bb620425d9b1c9861429d4a7e0faf937e294e2ea
Marcobc-exe <marco.developing@gmail.com>
2023-11-27 22:41:23 -0300

    Fixe(General): Handling datas map

--- client/src/components/Map/MapCanvas.tsx
+++ client/src/components/Map/MapCanvas.tsx
@@ -1,88 +1,109 @@
 import DeckGl from "@deck.gl/react/typed";
 import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { MAP_STYLE, MAPBOX_TOKEN } from '../../config/configMap.ts'
-import { Dispatch, SetStateAction, Suspense, useState } from "react";
-import useMap from "../../hooks/controllers/useMap.ts";
+import { MAP_STYLE, MAPBOX_TOKEN } from "../../config/configMap.ts";
+import { Suspense, useState } from "react";
+// import useMap from "../../hooks/controllers/useMap.ts";
 import { MapType } from "../../types/Map-types/types";
-import useAreas from "../../hooks/controllers/useAreas.ts";
-import { generateHighlightsLayersMap, generateLayersMap } from "../../utils/layers.ts";
+// import useAreas from "../../hooks/controllers/useAreas.ts";
+import {
+  generateHighlightsLayersMap,
+  generateLayersMap,
+} from "../../utils/layers.ts";
 import { handleCursorMap } from "../../utils/handleCursorMap.ts";
-import { handleDeviceTooltip, handleTooltip } from "../../utils/handleTooltipMap.ts";
+import {
+  handleDeviceTooltip,
+  handleTooltip,
+} from "../../utils/handleTooltipMap.ts";
 import FilterButtons from "../FilterButtons/FilterButtons.tsx";
-import { useMarkers } from "../../hooks/controllers/useUnits.ts";
-import { generateMarkers } from '../../utils/markers.ts';
+// import { useMarkers } from "../../hooks/controllers/useUnits.ts";
+import { generateMarkers } from "../../utils/markers.ts";
+
+import { map } from "../../data/map/map.ts";
+import { areas } from "../../data/areas/areas.ts";
+import { unitMarkers } from "../../data/unitsMarkers/unitsMarkers.ts";
 
 const STYLE_MAP = {
-	height: "calc(100vh - 180px)",
-	width: "100%",
-	position: "relative",
-  borderRadius: "0 0 10px 0"
+  height: "calc(100vh - 180px)",
+  width: "100%",
+  position: "relative",
+  borderRadius: "0 0 10px 0",
 };
 
 export const MapCanvas = () => {
-  const { dataMap, errorMap, loadingMap } = useMap();
-  const { dataAreas, errorAreas, loadingAreas } = useAreas();
-  const { dataMarkers, errorMarkers, loadingMarkers } = useMarkers();
+  // const { dataMap, errorMap, loadingMap } = useMap();
+  // const { dataAreas, errorAreas, loadingAreas } = useAreas();
+  // const { dataMarkers, errorMarkers, loadingMarkers } = useMarkers();
 
   type StateFilter = [boolean, (newFilter: boolean) => void];
-  
-  const [filterByIrrigating, setFilterByIrrigating]: StateFilter = useState(false);
-  const [filterByCrop, setFilterByCrop]: StateFilter = useState(false)
-  const [showUnits, setShowUnits]: StateFilter = useState(false)
-  const [showFailures, setShowFailures]: StateFilter = useState(false)
+
+  const [filterByIrrigating, setFilterByIrrigating]: StateFilter =
+    useState(false);
+  const [filterByCrop, setFilterByCrop]: StateFilter = useState(false);
+  const [showUnits, setShowUnits]: StateFilter = useState(false);
+  const [showFailures, setShowFailures]: StateFilter = useState(false);
 
   const [highlights, setHighlights] = useState([]);
 
-  if (loadingMap || loadingAreas || loadingMarkers) return <h2>Loading map...</h2>
-  if (errorMap) return <p>{errorMap}</p>
-  if (errorAreas) return <p>{errorAreas}</p>
-  if (errorMarkers) return <p>{errorMarkers}</p>
+  // if (loadingMap || loadingAreas || loadingMarkers)
+    // return <h2>Loading map...</h2>;
+  // if (errorMap) return <p>{errorMap}</p>;
+  // if (errorAreas) return <p>{errorAreas}</p>;
+  // if (errorMarkers) return <p>{errorMarkers}</p>;
 
   function getInitialViewState(dataMap: MapType) {
     const coordinates: string[] = dataMap.center.split(";");
     const lat: number = Number.parseFloat(coordinates[0]);
     const lng: number = Number.parseFloat(coordinates[1]);
     const zoom: number = dataMap.zoom;
-  
+
     return {
       latitude: lat,
       longitude: lng,
       zoom,
       minZoom: 13,
       maxZoom: 17,
-    }
+    };
   }
 
   const handleIrrigating = () => {
-    if (filterByCrop) setFilterByCrop(false)
-    if (showFailures) setShowFailures(false)
-    setFilterByIrrigating((currentValue) => !currentValue)
-  }
+    if (filterByCrop) setFilterByCrop(false);
+    if (showFailures) setShowFailures(false);
+    setFilterByIrrigating((currentValue) => !currentValue);
+  };
 
   const handleCrops = () => {
-    if (filterByIrrigating) setFilterByIrrigating(false)
-    if (showFailures) setShowFailures(false)
-    setFilterByCrop((currentValue) => !currentValue)
-  }
+    if (filterByIrrigating) setFilterByIrrigating(false);
+    if (showFailures) setShowFailures(false);
+    setFilterByCrop((currentValue) => !currentValue);
+  };
 
   const handleFailure = () => {
-    if (filterByIrrigating) setFilterByIrrigating(false)
-    if (filterByCrop) setFilterByCrop(false)
-    setShowFailures((currentValue) => !currentValue)
-  }
+    if (filterByIrrigating) setFilterByIrrigating(false);
+    if (filterByCrop) setFilterByCrop(false);
+    setShowFailures((currentValue) => !currentValue);
+  };
 
   const handleUnits = () => {
     setShowUnits((currentValue) => !currentValue);
-  }
+  };
 
-  return(
+  return (
     <Suspense fallback={<h2>Loading map...</h2>}>
       <DeckGl
-        initialViewState={getInitialViewState(dataMap)}
+        initialViewState={getInitialViewState(map)}
         controller={true}
         style={STYLE_MAP}
-        layers={[generateLayersMap(dataAreas, filterByIrrigating, filterByCrop, showFailures), showUnits && generateMarkers(dataMarkers), highlights]}
+        layers={[
+          generateLayersMap(
+            areas,
+            filterByIrrigating,
+            filterByCrop,
+            showFailures
+          ),
+          showUnits && generateMarkers(unitMarkers),
+          highlights,
+        ]}
         getCursor={(event) => handleCursorMap(event)}
         getTooltip={(info) => {
           if (info.object) {
@@ -97,7 +118,7 @@ export const MapCanvas = () => {
           if (event.object) {
             if (event.object.coordinates) return;
 
-            const sectorsHighlights = generateHighlightsLayersMap(dataAreas, event.object) // set highlights
+            const sectorsHighlights = generateHighlightsLayersMap(areas, event.object) // set highlights
             setHighlights(sectorsHighlights);
 
           } else {
@@ -114,9 +135,9 @@ export const MapCanvas = () => {
           minZoom={13}
           maxZoom={17}
         />
-        <FilterButtons 
-          handleIrrigating={handleIrrigating} 
-          handleCrops={handleCrops} 
+        <FilterButtons
+          handleIrrigating={handleIrrigating}
+          handleCrops={handleCrops}
           handleUnits={handleUnits}
           handleFailure={handleFailure}
           filterByIrrigating={filterByIrrigating}
@@ -126,5 +147,5 @@ export const MapCanvas = () => {
         />
       </DeckGl>
     </Suspense>
-  )
-}
+  );
+};
